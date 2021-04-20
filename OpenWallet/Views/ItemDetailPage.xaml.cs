23fa2769785fb35e8b6b990e7fff20a63c11f664
Simon Marchal <kertyxp@gmail.com>
2021-04-20 15:18:52 +0200

    Init Commit

---
+++ OpenWallet/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using OpenWallet.ViewModels;
+
+namespace OpenWallet.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
