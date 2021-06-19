7ff4b0926435682bf1268f1f48e5e776346f574c
Simon Marchal <kertyxp@gmail.com>
2021-06-19 14:19:02 +0200

    move common logic

--- OpenWallet.WinForm/Form1.cs
+++ OpenWallet.WinForm/Form1.cs
@@ -28,210 +28,44 @@ namespace OpenWallet.WinForm
         private async void Form1_Load(object sender, EventArgs e)
         {
 
-            await Task.Delay(1000);
-            GlobalConfig oGlobalConfig = null;
-            if (File.Exists("config.json"))
-            {
-                oGlobalConfig = JsonConvert.DeserializeObject<GlobalConfig>(File.ReadAllText("config.json"));
-            }
-            else
-            {
-                oGlobalConfig = new GlobalConfig();
-                oGlobalConfig.aConfigs = new List<ExchangeConfig>()
-                {
-                    new ExchangeConfig()
-                    {
-                        ExchangeCode = "Binance",
-                        ApiKey = "ApiKey",
-                        SecretKey = "SecretKey",
-                        AdditionnalKey = ""
-                    }
-                };
-                File.WriteAllText("config.json", JsonConvert.SerializeObject(oGlobalConfig));
-            }
-            oGlobalConfig.FiatMoneys = oGlobalConfig.FiatMoneys.GroupBy(c => c).Select(c => c.FirstOrDefault()).ToList();
+            var aExchanges = Config.LoadExchanges();
 
-            List<IExchange> aEchanges = new List<IExchange>();
-            var type = typeof(IExchange);
-            var types = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(s => s.GetTypes())
-                .Where(p => type.IsAssignableFrom(p) && p.IsClass)
-                .ToList()
-                .Select(t => Activator.CreateInstance(t) as IExchange)
-                .Where(t => t != null);
+            List<CurrencySymbolPrice> aAllCurrencies = Config.GetCurrencries(aExchanges);
 
-            foreach (var oConfig in oGlobalConfig.aConfigs)
-            {
-                var typeExchange = types.FirstOrDefault(t => t.ExchangeCode == oConfig.ExchangeCode);
-                if (typeExchange == null)
-                    continue; // oops
+            var aAll = await Config.GetBalances(aExchanges, aAllCurrencies);
 
-                var oExchange = Activator.CreateInstance(typeExchange.GetType()) as IExchange;
-                if (oExchange == null)
-                    continue; // re-oops
+            InsertCurrentBalanceInGrid(aAll);
 
-                oExchange.Init(oGlobalConfig, oConfig);
-                aEchanges.Add(oExchange);
-            }
+
+            List<GlobalTrade> aListTrades = await Config.LoadTrades(aExchanges, aAllCurrencies);
 
 
 
-            var aTasks = aEchanges.Select(oExchange => Task.Run(oExchange.GetBalance)).ToList();
-            List<GlobalBalance> aAll = new List<GlobalBalance>();
-
-            //foreach (var oTask in aTasks)
-            //{
-            //    if(oTask.Status == TaskStatus.WaitingForActivation)
-            //    {
-            //        oTask.Start();
-            //    }
-            //}
-
-            foreach (var oTask in aTasks)
-            {
-                var aBalance = oTask.GetAwaiter().GetResult();
-                aAll.AddRange(aBalance);
-            }
-
-            List<CurrencySymbolPrice> aAllCurrencies = new List<CurrencySymbolPrice>();
-            var aTaskCurrencies = aEchanges.Select(oExchange => Task.Run(oExchange.GetCurrencies)).ToList();
-
-            foreach (var oTask in aTaskCurrencies)
-            {
-                var aBalance = oTask.GetAwaiter().GetResult();
-                aAllCurrencies.AddRange(aBalance);
-            }
-
-            List<CurrencySymbolPrice> aFiatisation = new List<CurrencySymbolPrice>();
-            // fiatisation (needs at least 2 fiats Moneys (first is the fiatisation TO)
-            if (oGlobalConfig.FiatMoneys.Count() > 1)
-            {
-                string sTo = oGlobalConfig.FiatMoneys.FirstOrDefault();
-                aFiatisation = oGlobalConfig.FiatMoneys.Skip(1).Select(fiat =>
-                {
-                    return new CurrencySymbolPrice()
-                    {
-                        From = fiat,
-                        To = sTo,
-                        Price = aAllCurrencies.GetCustomPrice(fiat, sTo)
-                    };
-                }).ToList();
-            }
-
-            aAll.ForEach(b =>
-            {
-                b.BitCoinValue = aAllCurrencies.GetBtcValue(b);
-                b.FavCrypto = oGlobalConfig.FavoriteCurrency;
-                b.FavCryptoValue = aAllCurrencies.GetCustomValue(b, b.FavCrypto);
-
-                dataGridView1.Rows.Add(b.Exchange, b.Crypto, b.Value, b.BitCoinValue, b.FavCryptoValue);
-            });
-
-            dataGridView1.Columns[4].HeaderText = oGlobalConfig.FavoriteCurrency;
-
-            var dTotalSumBtc = aAll.Sum(a => a.BitCoinValue);
-            var dTotalSum = aAll.Sum(a => a.FavCryptoValue);
-
-            dataGridView1.Rows.Insert(0, "TOTAL", oGlobalConfig.FavoriteCurrency, dTotalSum, dTotalSumBtc, dTotalSum);
-
-
-
-
-            var aTasks3 = aEchanges.Select(oExchange =>
-            {
-                string sFileName = "Trades_" + oExchange.ExchangeName + ".json";
-
-                var aTrades = File.Exists(sFileName) ? JsonConvert.DeserializeObject<List<GlobalTrade>>(File.ReadAllText(sFileName)) : new List<GlobalTrade>();
-                return Task.Run(() => oExchange.GetTradeHistory(aTrades));
-            }).ToList();
-            List<GlobalTrade> aListTrades = new List<GlobalTrade>();
-
-            foreach (var oTask in aTasks3)
-            {
-                try
-                {
-                    var aBalance = await oTask;
-                    if (aBalance.Any() == false)
-                        continue;
-
-                    File.WriteAllText("Trades_" + aBalance?.FirstOrDefault()?.Exchange + ".json", JsonConvert.SerializeObject(aBalance));
-
-                    aListTrades.AddRange(aBalance);
-                }
-                catch
-                {
-
-                }
-            }
-
-            aListTrades.ForEach(trade =>
-            {
-                var fiatFrom = aFiatisation.FirstOrDefault(f => f.From == trade.From);
-                var fiatTo = aFiatisation.FirstOrDefault(f => f.From == trade.To);
-                if (fiatFrom != null && fiatTo != null)
-                    return;
-
-                if (fiatFrom != null)
-                {
-                    trade.From = fiatFrom.To;
-                    trade.Price = trade.Price / fiatFrom.Price;
-                    trade.QuantityFrom = trade.QuantityFrom * fiatFrom.Price;
-                    return;
-                }
-
-                if (fiatTo != null)
-                {
-                    trade.To = fiatTo.To;
-                    trade.Price = trade.Price * fiatTo.Price;
-                    trade.QuantityTo = trade.QuantityTo * fiatTo.Price;
-                    return;
-                }
-            });
-
-
-            var aListTrades2 = aListTrades.GroupBy(l => l.From + "|" + l.To + "_" + l.dtTrade.ToString("_yyyy-MM-dd")).Select(l =>
-            {
-                var one = l.FirstOrDefault();
-                var oGlobalTrade = new GlobalTrade();
-                oGlobalTrade.Exchange = one.Exchange;
-                oGlobalTrade.From = one.From;
-                oGlobalTrade.To = one.To;
-                oGlobalTrade.QuantityFrom = l.Sum(m => m.QuantityFrom);
-                oGlobalTrade.QuantityTo = l.Sum(m => m.QuantityTo);
-                oGlobalTrade.InternalExchangeId = one.InternalExchangeId;
-                oGlobalTrade.dtTrade = one.dtTrade;
-
-                oGlobalTrade.Price = oGlobalTrade.QuantityFrom / oGlobalTrade.QuantityTo;
-
-                return oGlobalTrade;
-            }).OrderBy(l => l.dtTrade).ToList();
-
-            aListTrades2.OrderBy(t => t.Couple).ToList().ForEach(t =>
+            Config.ConvertTradesToDailyTrades(aListTrades).ForEach(t =>
             {
                 dgv_trade_day.Rows.Add(t.Exchange, t.Couple, t.From, t.QuantityFrom, t.To, t.QuantityTo, t.dtTrade.ToString("yyyy-MM-dd"));
             });
 
-            var aListTrades3 = aListTrades.GroupBy(l => l.From + "|" + l.To).Select(l =>
-            {
-                var one = l.FirstOrDefault();
-                var oGlobalTrade = new GlobalTrade();
-                oGlobalTrade.Exchange = one.Exchange;
-                oGlobalTrade.From = one.From;
-                oGlobalTrade.To = one.To;
-                oGlobalTrade.QuantityFrom = l.Sum(m => m.QuantityFrom);
-                oGlobalTrade.QuantityTo = l.Sum(m => m.QuantityTo);
-                oGlobalTrade.InternalExchangeId = one.InternalExchangeId;
-                oGlobalTrade.dtTrade = one.dtTrade;
 
-                oGlobalTrade.Price = oGlobalTrade.QuantityFrom / oGlobalTrade.QuantityTo;
-
-                return oGlobalTrade;
-            }).OrderBy(l => l.From).ToList();
-
-            aListTrades3.OrderBy(t => t.Couple).ToList().ForEach(t =>
+            Config.ConvertTradesToGlobalTrades(aListTrades).ForEach(t =>
             {
                 dgv_Trades.Rows.Add(t.Exchange, t.Couple, t.From, t.QuantityFrom, t.To, t.QuantityTo);
             });
+        }
+
+        private void InsertCurrentBalanceInGrid(List<GlobalBalance> aAll)
+        {
+            foreach (var b in aAll)
+            {
+                dataGridView1.Rows.Add(b.Exchange, b.Crypto, b.Value, b.BitCoinValue, b.FavCryptoValue);
+            }
+
+
+            var dTotalSumBtc = aAll.Sum(a => a.BitCoinValue);
+            var dTotalSum = aAll.Sum(a => a.FavCryptoValue);
+
+            dataGridView1.Columns[4].HeaderText = Config.oGlobalConfig.FavoriteCurrency;
+            dataGridView1.Rows.Insert(0, "TOTAL", Config.oGlobalConfig.FavoriteCurrency, dTotalSum, dTotalSumBtc, dTotalSum);
         }
     }
 
---
+++ OpentWallet.Logic/Config.cs
@@ -0,0 +1,245 @@
+﻿using Newtonsoft.Json;
+using OpenWallet.Common;
+using OpenWallet.Logic.Abstraction;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpentWallet.Logic
+{
+    public class Config
+    {
+        public static GlobalConfig oGlobalConfig = LoadConfig();
+        private static GlobalConfig LoadConfig()
+        {
+
+            GlobalConfig oConfig = null;
+            if (File.Exists("config.json"))
+            {
+                oConfig = JsonConvert.DeserializeObject<GlobalConfig>(File.ReadAllText("config.json"));
+            }
+            else
+            {
+                oConfig = new GlobalConfig();
+                oConfig.aConfigs = new List<ExchangeConfig>()
+                {
+                    new ExchangeConfig()
+                    {
+                        ExchangeCode = "Binance",
+                        ApiKey = "ApiKey",
+                        SecretKey = "SecretKey",
+                        AdditionnalKey = ""
+                    }
+                };
+                File.WriteAllText("config.json", JsonConvert.SerializeObject(oConfig));
+            }
+            oConfig.FiatMoneys = oConfig.FiatMoneys.GroupBy(c => c).Select(c => c.FirstOrDefault()).ToList();
+            return oConfig;
+        }
+
+        public static List<IExchange> LoadExchanges()
+        {
+
+            List<IExchange> aExchanges = new List<IExchange>();
+            var type = typeof(IExchange);
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => type.IsAssignableFrom(p) && p.IsClass)
+                .ToList()
+                .Select(t => Activator.CreateInstance(t) as IExchange)
+                .Where(t => t != null);
+
+            foreach (var oConfig in Config.oGlobalConfig.aConfigs)
+            {
+                var typeExchange = types.FirstOrDefault(t => t.ExchangeCode == oConfig.ExchangeCode);
+                if (typeExchange == null)
+                    continue; // oops
+
+                var oExchange = Activator.CreateInstance(typeExchange.GetType()) as IExchange;
+                if (oExchange == null)
+                    continue; // re-oops
+
+                oExchange.Init(Config.oGlobalConfig, oConfig);
+                aExchanges.Add(oExchange);
+            }
+
+            return aExchanges;
+        }
+
+        public static List<CurrencySymbolPrice> GetCurrencries(List<IExchange> aExchanges)
+        {
+
+            List<CurrencySymbolPrice> aAllCurrencies = new List<CurrencySymbolPrice>();
+            var aTaskCurrencies = aExchanges.Select(oExchange => Task.Run(oExchange.GetCurrencies)).ToList();
+
+            foreach (var oTask in aTaskCurrencies)
+            {
+                var aBalance = oTask.GetAwaiter().GetResult();
+                aAllCurrencies.AddRange(aBalance);
+            }
+
+            return aAllCurrencies;
+        }
+
+        public static List<CurrencySymbolPrice> LoadFiatisation(List<CurrencySymbolPrice> aAllCurrencies)
+        {
+
+            List<CurrencySymbolPrice> aFiatisation = new List<CurrencySymbolPrice>();
+            // fiatisation (needs at least 2 fiats Moneys (first is the fiatisation TO)
+            if (oGlobalConfig.FiatMoneys.Count() > 1)
+            {
+                string sTo = Config.oGlobalConfig.FiatMoneys.FirstOrDefault();
+                aFiatisation = Config.oGlobalConfig.FiatMoneys.Skip(1).Select(fiat =>
+                {
+                    return new CurrencySymbolPrice()
+                    {
+                        From = fiat,
+                        To = sTo,
+                        Price = aAllCurrencies.GetCustomPrice(fiat, sTo)
+                    };
+                }).ToList();
+            }
+
+            return aFiatisation;
+        }
+
+        public static async Task<List<GlobalBalance>> GetBalances(List<IExchange> aExchanges, List<CurrencySymbolPrice> aAllCurrencies)
+        {
+
+            ConcurrentQueue<GlobalBalance> aAll = new ConcurrentQueue<GlobalBalance>();
+
+            var aTasks = aExchanges.Select(oExchange => Task.Run(() =>
+            {
+                var aBalance = oExchange.GetBalance();
+                foreach (var oBalance in aBalance)
+                {
+                    oBalance.BitCoinValue = aAllCurrencies.GetBtcValue(oBalance);
+                    oBalance.FavCrypto = Config.oGlobalConfig.FavoriteCurrency;
+                    oBalance.FavCryptoValue = aAllCurrencies.GetCustomValue(oBalance, oBalance.FavCrypto);
+                    aAll.Enqueue(oBalance);
+                }
+            })).ToList();
+
+            foreach (var oTask in aTasks)
+            {
+                await oTask;
+            }
+
+            return aAll.ToList();
+        }
+
+        public static async Task<List<GlobalTrade>> LoadTrades(List<IExchange> aExchanges, List<CurrencySymbolPrice> aAllCurrencies)
+        {
+
+            List<CurrencySymbolPrice> aFiatisation = Config.LoadFiatisation(aAllCurrencies);
+
+            var aTasks3 = aExchanges.Select(oExchange =>
+            {
+                string sFileName = "Trades_" + oExchange.ExchangeName + ".json";
+
+                var aTrades = File.Exists(sFileName) ? JsonConvert.DeserializeObject<List<GlobalTrade>>(File.ReadAllText(sFileName)) : new List<GlobalTrade>();
+                return Task.Run(() => oExchange.GetTradeHistory(aTrades));
+            }).ToList();
+            List<GlobalTrade> aListTrades = new List<GlobalTrade>();
+
+            foreach (var oTask in aTasks3)
+            {
+                try
+                {
+                    var aBalance = await oTask;
+                    if (aBalance.Any() == false)
+                        continue;
+
+                    File.WriteAllText("Trades_" + aBalance?.FirstOrDefault()?.Exchange + ".json", JsonConvert.SerializeObject(aBalance));
+
+                    aListTrades.AddRange(aBalance);
+                }
+                catch
+                {
+
+                }
+            }
+
+
+            aListTrades.ForEach(trade =>
+            {
+                FiatiseOneTrade(trade, aFiatisation);
+            });
+
+            return aListTrades;
+
+
+        }
+
+        private static void FiatiseOneTrade(GlobalTrade oTrade, List<CurrencySymbolPrice> aFiatisation)
+        {
+            var fiatFrom = aFiatisation.FirstOrDefault(f => f.From == oTrade.From);
+            var fiatTo = aFiatisation.FirstOrDefault(f => f.From == oTrade.To);
+            if (fiatFrom != null && fiatTo != null)
+                return;
+
+            if (fiatFrom != null)
+            {
+                oTrade.From = fiatFrom.To;
+                oTrade.Price = oTrade.Price / fiatFrom.Price;
+                oTrade.QuantityFrom = oTrade.QuantityFrom * fiatFrom.Price;
+                return;
+            }
+
+            if (fiatTo != null)
+            {
+                oTrade.To = fiatTo.To;
+                oTrade.Price = oTrade.Price * fiatTo.Price;
+                oTrade.QuantityTo = oTrade.QuantityTo * fiatTo.Price;
+                return;
+            }
+        }
+
+        public static List<GlobalTrade> ConvertTradesToDailyTrades(List<GlobalTrade> aListTrades)
+        {
+
+            return aListTrades.GroupBy(l => l.From + "|" + l.To + "_" + l.dtTrade.ToString("_yyyy-MM-dd")).Select(l =>
+            {
+                var one = l.FirstOrDefault();
+                var oGlobalTrade = new GlobalTrade();
+                oGlobalTrade.Exchange = one.Exchange;
+                oGlobalTrade.From = one.From;
+                oGlobalTrade.To = one.To;
+                oGlobalTrade.QuantityFrom = l.Sum(m => m.QuantityFrom);
+                oGlobalTrade.QuantityTo = l.Sum(m => m.QuantityTo);
+                oGlobalTrade.InternalExchangeId = one.InternalExchangeId;
+                oGlobalTrade.dtTrade = one.dtTrade;
+
+                oGlobalTrade.Price = oGlobalTrade.QuantityFrom / oGlobalTrade.QuantityTo;
+
+                return oGlobalTrade;
+            }).OrderBy(l => l.dtTrade).OrderBy(t => t.Couple).ToList();
+
+        }
+
+
+        public static List<GlobalTrade> ConvertTradesToGlobalTrades(List<GlobalTrade> aListTrades)
+        {
+            return aListTrades.GroupBy(l => l.From + "|" + l.To).Select(l =>
+            {
+                var one = l.FirstOrDefault();
+                var oGlobalTrade = new GlobalTrade();
+                oGlobalTrade.Exchange = one.Exchange;
+                oGlobalTrade.From = one.From;
+                oGlobalTrade.To = one.To;
+                oGlobalTrade.QuantityFrom = l.Sum(m => m.QuantityFrom);
+                oGlobalTrade.QuantityTo = l.Sum(m => m.QuantityTo);
+                oGlobalTrade.InternalExchangeId = one.InternalExchangeId;
+                oGlobalTrade.dtTrade = one.dtTrade;
+
+                oGlobalTrade.Price = oGlobalTrade.QuantityFrom / oGlobalTrade.QuantityTo;
+
+                return oGlobalTrade;
+            }).OrderBy(l => l.From).OrderBy(t => t.Couple).ToList();
+        }
+    }
+}
